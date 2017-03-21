2c2922290221dee4f848d0c9b7ba0e669e97b195
austi <austi@DESKTOP-C3QSPLJ.mshome.net>
2017-03-21 16:45:18 -0400

    interfaces and required extra files

--- get/src/get/IMapCreator.java
+++ get/src/get/IMapCreator.java
@@ -2,6 +2,8 @@ package get;
 
 import java.io.IOException;
 
+import edu.wmich.cs1120.la5.TerrainScanner;
+
 public interface IMapCreator {
 	TerrainScanner getScanner();
 	void scanTerrain(String fileName, int threshold) throws IOException;
--- get/src/get/test.java
+++ get/src/get/test.java
@@ -1,13 +1,11 @@
 package get;
 
+import edu.wmich.cs1120.la5.gui.MainWindow;
+
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("test one");
-
-		System.out.println("test two");
-		System.out.println("test three");
-		
+		MainWindow.startGUI();
 
 	}
 
