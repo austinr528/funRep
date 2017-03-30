153498b02c35c319db376c8e6a250083190ce4b2
austi <austi@10.80.144.232>
2017-03-30 12:40:50 -0400

    all analyzers finished

--- get/src/edu/wmich/cs1120/la5/EnergyAnalyzer.java
+++ get/src/edu/wmich/cs1120/la5/EnergyAnalyzer.java
@@ -3,35 +3,46 @@ package edu.wmich.cs1120.la5;
 import java.util.ArrayList;
 
 public class EnergyAnalyzer implements IRover {
+	private ArrayList<IArea> path;
+	private String analysis;
 
 	@Override
 	public ArrayList<IArea> getPath() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return path;
 	}
 
 	@Override
 	public void setPath(ArrayList<IArea> path) {
-		// TODO Auto-generated method stub
+		this.path = path;
 
 	}
 
 	@Override
 	public void analyzePath() {
-		// TODO Auto-generated method stub
+		double ene = 0;
+		for(int i=0; i<path.size(); i++){
+			ene += path.get(i).calcConsumedEnergy();
+		}
+		
+		analysis = Double.toString(ene);
 
 	}
 
 	@Override
 	public String getAnalysis() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return analysis;
 	}
 
 	@Override
 	public void setAnalysis(String analysis) {
-		// TODO Auto-generated method stub
+		this.analysis = analysis;
 
+	}
+	
+	public String toString(){
+		return analysis;
 	}
 
 }
