945e0582c20c060018182fe3168e476b3e6ded09
austi <austi@10.80.144.232>
2017-03-30 15:37:32 -0400

    binexp and literal

--- get/src/edu/wmich/cs1120/la5/BinaryExpression.java
+++ get/src/edu/wmich/cs1120/la5/BinaryExpression.java
@@ -1,6 +1,15 @@
 package edu.wmich.cs1120.la5;
 
 public class BinaryExpression implements IExpression {
+	private int left;
+	private int right;
+	private char c;
+	BinaryExpression(Literal left, Literal right, char c){
+		this.left = left.getValue();
+		this.right = right.getValue();
+		this.c = c;
+		
+	}
 
 	@Override
 	public Integer getValue() {
--- get/src/edu/wmich/cs1120/la5/Literal.java
+++ get/src/edu/wmich/cs1120/la5/Literal.java
@@ -1,11 +1,15 @@
 package edu.wmich.cs1120.la5;
 
 public class Literal implements IExpression {
+	private int num;
+	Literal(int num){
+		this.num = num;
+	}
 
 	@Override
 	public Integer getValue() {
 		// TODO Auto-generated method stub
-		return null;
+		return num;
 	}
 
 }
